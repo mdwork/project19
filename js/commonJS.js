ece98a9dc40aa318e4f4275dd662953f7f1c1df8
Zhe1ka <zheka.dzecina@gmail.com>
2015-03-26 08:15:36 +0200

    Zhe1ka

--- js/commonJS.js
+++ js/commonJS.js
@@ -30,4 +30,38 @@ $(document).ready(function() {
             $this.width(widthListCarts);
         }
     });
+
+    var listContact = $('.list-cart-contact'),
+        listInfoMore = $('.list-more-info');
+
+    function addActiveClass(list) {
+        list.each(function () {
+            var lenListCartMoreInfo = $(this).children('li').length / 2,
+                fixNumber = lenListCartMoreInfo.toFixed();
+
+            $(this).children('li').eq(fixNumber - 1).addClass('active');
+        });
+    }
+
+    addActiveClass(listContact);
+    addActiveClass(listInfoMore);
+
+
+    /*animate home slider*/
+    var itemServiceBlock = $('.list-cart-contact li');
+
+    itemServiceBlock.on('click', function(e){
+        e.preventDefault();
+
+        var curItem = $(this).index();
+
+        $(this).parent().children().removeClass('active');
+
+        $(this).addClass('active');
+
+        $(this).parent().parent().children('.list-more-info').children('li').removeClass('active');
+        $(this).parent().parent().children('.list-more-info').children('li').eq(curItem).addClass('active');
+
+
+    });
 });
