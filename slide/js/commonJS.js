0234efd7eb16d2f0343fd72b39273a47b8c872c2
Zhe1ka <zheka.dzecina@gmail.com>
2015-04-20 14:22:29 +0300

    Zhe1ka

---
+++ slide/js/commonJS.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+    $('.list-cart').each(function(){
+        var $this = $(this),
+            len = $this.children('li').length;
+
+        if(len < 10) {
+            var cntItems = len / 2,
+                widthListCarts = Math.round(cntItems) * 212;
+
+            $this.width(widthListCarts);
+        }
+    });
+
+    var listContact = $('.toggle-active'),
+        listInfoMore = $('.list-more-info');
+
+    function addActiveClass(list) {
+        list.each(function () {
+            var lenListCartMoreInfo = $(this).children('li').length / 2,
+                fixNumber = lenListCartMoreInfo.toFixed();
+
+            $(this).children('li').eq(fixNumber - 1).addClass('active');
+        });
+    }
+
+    addActiveClass(listContact);
+    addActiveClass(listInfoMore);
+
+
+    /*animate home slider*/
+    var itemServiceBlock = $('.toggle-active li');
+
+    itemServiceBlock.on('click', function(e){
+        e.preventDefault();
+
+        var curItem = $(this).index();
+
+        $(this).parent().children().removeClass('active');
+
+        $(this).addClass('active');
+
+        $(this).parent().parent().children('.list-more-info').children('li').removeClass('active');
+        $(this).parent().parent().children('.list-more-info').children('li').eq(curItem).addClass('active');
+
+
+    });
+
+    $('')
+});
